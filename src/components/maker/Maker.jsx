07a9cd0801475ad1466f133790fac6e70d796019
khjoo1203 <khjoo1203@naver.com>
2022-09-22 13:19:08 +0900

    handle state change on editor

--- src/components/maker/Maker.jsx
+++ src/components/maker/Maker.jsx
@@ -6,70 +6,87 @@ import Editor from '../editor/Editor';
 import Preview from '../preview/Preview';
 import styles from './Maker.module.css';
 
-const Maker = ({authService}) => {
-  const [cards, setCards] = useState([
-    {
-        id: '1',
-        name: 'Ellie',
-        company: 'Samsung',
-        theme: 'dark',
-        title: 'Software Engineer',
-        email: 'ellie@gmail.com',
-        message: 'go for it',
-        fileName: 'ellie',
-        fileURL: null
+const Maker = ({ authService }) => {
+  const [cards, setCards] = useState({
+    1: {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
     },
-    {
-        id: '2',
-        name: 'Ellie2',
-        company: 'Samsung',
-        theme: 'light',
-        title: 'Software Engineer',
-        email: 'ellie@gmail.com',
-        message: 'go for it',
-        fileName: 'ellie',
-        fileURL: null
+    2: {
+      id: '2',
+      name: 'Ellie2',
+      company: 'Samsung',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
     },
-    {
-        id: '3',
-        name: 'Ellie3',
-        company: 'Samsung',
-        theme: 'colorful',
-        title: 'Software Engineer',
-        email: 'ellie@gmail.com',
-        message: 'go for it',
-        fileName: 'ellie',
-        fileURL: null
-    }
-  ]);  
-  const navigate = useNavigate();  
+    3: {
+      id: '3',
+      name: 'Ellie3',
+      company: 'Samsung',
+      theme: 'colorful',
+      title: 'Software Engineer',
+      email: 'ellie@gmail.com',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  });
+
+  const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
-    authService.onAuthChange(user => {
-        if(!user) {
-            navigate('/');
-        }
-    })
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate('/');
+      }
+    });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
-  }
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
+  };
 
   return (
     <section className={styles.maker}>
-        <Header onLogout={onLogout} />
-        <div className={styles.container}>
-            <Editor cards={cards} addCard={addCard} />
-            <Preview cards={cards} />
-        </div>
-        <Footer />
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
+        <Preview cards={cards} />
+      </div>
+      <Footer />
     </section>
-  )
-}
+  );
+};
 
 export default Maker;
